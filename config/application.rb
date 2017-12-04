f21077f31875907bc7c8921481dc24965dc2d65a
1412350 <ngoc120742@gmail.com>
2017-12-04 13:54:00 +0700

    fix config application

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,12 @@
 require_relative 'boot'
-
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -14,5 +21,9 @@ module BlockchainReact
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    Rails.application.routes.default_url_options[:host] = ENV['HOST']
+    # Don't generate system test files.
+    config.assets.enabled = true
+    config.assets.paths << Rails.root.join("app","assets","fonts")
   end
 end
